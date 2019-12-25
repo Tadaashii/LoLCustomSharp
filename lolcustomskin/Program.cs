4984aab0ef6c99c86d0ef7a502d85cca24e900c8
Crauzer <filip.quitko@gmail.com>
2019-12-25 08:57:48 +0100

    fixed main

--- lolcustomskin/Program.cs
+++ lolcustomskin/Program.cs
@@ -19,83 +19,13 @@ namespace lolcustomskin
         {
             string exePath = Assembly.GetEntryAssembly().Location;
             string configPath = Path.GetDirectoryName(exePath) + "/lolcustomskin-sharp.bin";
-            OverlayPatcher patcher = OverlayPatcher.Load();
+            OverlayPatcher patcher = new OverlayPatcher();
 
-            patcher.Prefix = args.Length > 0 ? args[0] : "MOD/";
             Console.WriteLine($"Overlay: {patcher.Prefix}");
             Console.WriteLine($"Offsets: 0x{patcher.Checksum:X08} 0x{patcher.FileProviderListOffset:X08} 0x{patcher.PMethArrayOffset:X08}");
-
             Console.WriteLine("Waiting for league to start...");
-            for(;;)
-            {
-                foreach(Process process in Process.GetProcessesByName("League of Legends"))
-                {
-                    if(!OverlayPatcher.IsLeague(process))
-                    {
-                        break;
-                    }
-                    Console.WriteLine("Found league!");
-                    bool offsetsUpdated = false;
 
-                    try
-                    {
-                        using (LeagueProcess league = new LeagueProcess(process))
-                        {
-                            if (patcher.NeedsUpdate(league))
-                            {
-                                Console.WriteLine("Offsets need to be updated!");
-                                while (process.MainWindowTitle != "League of Legends (TM) Client")
-                                {
-                                    Thread.Sleep(10);
-                                    process.Refresh();
-                                }
-                                Console.WriteLine("Updating offsets...");
-                                patcher.UpdateOffsets(league);
-                                offsetsUpdated = true;
-                            }
-                            else
-                            {
-                                offsetsUpdated = false;
-                            }
-                            Console.WriteLine("Patching offsets!");
-                            patcher.Patch(league);
-                        }
-                    }
-                    catch(Exception error)
-                    {
-                        Console.WriteLine(error.StackTrace);
-                        Console.WriteLine("Press enter to exit...");
-                        Console.ReadLine();
-                        return;
-                    }
-
-                    if(offsetsUpdated)
-                    {
-                        Console.WriteLine("Offsets have been updated!");
-                        Console.WriteLine($"Offsets: 0x{patcher.Checksum:X08} 0x{patcher.FileProviderListOffset:X08} 0x{patcher.PMethArrayOffset:X08}");
-                        try
-                        {
-                            using (FileStream file = new FileStream(configPath, FileMode.OpenOrCreate))
-                            {
-                                BinaryFormatter formatter = new BinaryFormatter();
-                                formatter.Serialize(file, patcher);
-                            }
-                        }
-                        catch (Exception error)
-                        {
-                            Console.WriteLine(error.StackTrace);
-                            Console.WriteLine("Press enter to exit...");
-                            Console.ReadLine();
-                            return;
-                        }
-                    }
-                    Console.WriteLine("Waiting for league to exit...");
-                    process.WaitForExit();
-                    Console.WriteLine("Waiting for league to start...");
-                    break;
-                }
-                Thread.Sleep(1000);
-            }
+            patcher.Start(args.Length > 0 ? args[0] : "MOD/");
         }
     }
 }
